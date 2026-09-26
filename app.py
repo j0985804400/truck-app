ae087c136f269a1a30dacc52253ed7c9ed75437d
j0985804400 <j0985804400@gmail.com>
2026-09-26 16:11:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 def init_state():
     st.session_state['truck_l'] = 820
-    st.session_state['truck_w'] = 243
+    st.session_state['truck_w'] = 240
     
     if 'reset_count' not in st.session_state:
         st.session_state['reset_count'] = 0
@@ -49,8 +49,8 @@ def reset_all():
         item['qty'] = 0
         item['required'] = False
 
-st.set_page_config(page_title="貨車裝箱防爆計算器", layout="centered")
-st.title("📦 貨車裝箱防爆計算器")
+st.set_page_config(page_title="貨車裝箱計算器", layout="centered")
+st.title("📦 貨車裝箱計算器")
 
 st.markdown(f"<p style='color: gray; margin-bottom: 5px;'>🚚 目前車斗規格：長 {st.session_state['truck_l']} cm × 寬 {st.session_state['truck_w']} cm</p>", unsafe_allow_html=True)
 
